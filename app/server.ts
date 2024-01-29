91694a0b793405665cc3b9e997352d512936abec
Denys <babychdenya@gmail.com>
2024-01-29 22:56:25 +0000

    chore: add procces.env and .env.example file

--- app/server.ts
+++ app/server.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose'
-
 import { app } from './app'
 
+const { PORT } = process.env
 const DB_HOST =
   'mongodb+srv://Denys:nZlqN2oa35ko1QqO@cluster0.83ohyqy.mongodb.net/my-contacts?retryWrites=true&w=majority'
 
 mongoose
   .connect(DB_HOST as string)
   .then(() => {
-    app.listen(3000, () =>
-      console.log(`Server running. Use our API on port: ${3000}`)
+    app.listen(PORT, () =>
+      console.log(`Server running. Use our API on port: ${PORT}`)
     )
   })
   .catch(e => {
