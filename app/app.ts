a2712e1f98e412ac3f7b1b0b216f8a6d859b0c9a
Andrii Malysh <andriiymalysh@gmail.com>
2024-02-01 15:51:47 +0200

    feat: add swagger documentation for auth routes

--- app/app.ts
+++ app/app.ts
@@ -6,6 +6,8 @@ import express, {
   type Response
 } from 'express'
 import logger from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 import { authRouter } from './routes/api/auth'
 import { moviesRouter } from './routes/api/movies-router'
@@ -23,6 +25,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/api/auth', authRouter)
 app.use('/api/movies', moviesRouter)
 
