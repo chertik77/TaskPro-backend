55959413c2a710635e94bd32616424210abbe40f
Andrii Malysh <andriiymalysh@gmail.com>
2024-02-04 15:59:48 +0200

    fix: chenge routers

--- app/routes/api/dashboard.ts
+++ app/routes/api/dashboard.ts
@@ -13,15 +13,15 @@ export const dashboardRouter = express.Router()
 
 dashboardRouter.use(authenticate)
 
-// Dashboard
+//! Dashboard
 dashboardRouter.patch('/') // Switch theme
 
 dashboardRouter.post('/help') // Send email 'Need help'
 
-// Boards
+//! Boards
 dashboardRouter.get('/', dashboardController.getAll) // Get all boards
 
-// dashboardRouter.get('/:boardName', dashboardController.getById) // Get board for boardName
+dashboardRouter.get('/:boardName') // Get board for boardName
 
 dashboardRouter.post('/', validateBody(boardSchema.addNewBoardSchema), dashboardController.add) // Add new board
 
@@ -29,23 +29,15 @@ dashboardRouter.patch('/:boardName', validateBody(boardSchema.editBoardSchema), 
 
 dashboardRouter.delete('/:boardName', dashboardController.deleteById) // Delete board
 
-// Columns
-dashboardRouter.get('/:boardName/', columnController.getAll) // Get all columns of the board
-
-// dashboardRouter.get('/:boardName/:columnId', isValidId, columnController.getById) // Get column for id
-
-dashboardRouter.post('/:boardName/', validateBody(addColumnSchema), columnController.add) // Add new column
+//! Columns
+dashboardRouter.post('/:boardName', validateBody(addColumnSchema), columnController.add) // Add new column
 
 dashboardRouter.patch('/:boardName/:columnId', isValidId, validateBody(editColumnSchema), columnController.updateById) // Edit column
 
 dashboardRouter.delete('/:boardName/:columnId', isValidId, columnController.deleteById) // Delete column
 
-// Tasks
-dashboardRouter.get('/:boardName/tasks', taskController.getAll) // Get all tasks of the board
-
-// dashboardRouter.get('/:boardName/:columnId/:taskId') // Get task for id
-
-dashboardRouter.post('/:boardName/:columnId/', isValidId, validateBody(addNewTaskSchema), taskController.add) // Add new task
+//! Tasks
+dashboardRouter.post('/:boardName/:columnId', isValidId, validateBody(addNewTaskSchema), taskController.add) // Add new task
 
 dashboardRouter.patch('/:boardName/:columnId/:taskId', isValidId, validateBody(editTaskSchema), taskController.updateById) // Edit task
 
