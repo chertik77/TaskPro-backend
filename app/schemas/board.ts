bab3d884b1cfc81186bcd696c863a0dcaf5789c5
chertik77 <denysbabych.dev@gmail.com>
2024-12-15 17:11:00 +0000

    feat: add board icons to prisma schema

--- app/schemas/board.ts
+++ app/schemas/board.ts
@@ -1,16 +1,6 @@
+import { Icons } from '@prisma/client'
 import boardImages from 'data/board-bg-images.json'
 import * as z from 'zod'
-
-export const icons = [
-  'project',
-  'star',
-  'loading',
-  'puzzle',
-  'container',
-  'lightning',
-  'colors',
-  'hexagon'
-] as const
 
 function zodEnumFromObjKeys<K extends string>(
   obj: Record<K, unknown>
@@ -23,7 +13,7 @@ export const zodObjectId = () => z.string().regex(/^[0-9a-f]{24}$/)
 
 export const AddBoardSchema = z.object({
   title: z.string().min(3),
-  icon: z.enum(icons),
+  icon: z.nativeEnum(Icons),
   background: zodEnumFromObjKeys(boardImages)
 })
 
