5134d79859ee7d65a519af5a8c753f6891a3d4e9
chertik77 <denysbabych.dev@gmail.com>
2025-03-07 15:59:52 +0000

    fix: minor updates & improvements

--- app/config/nodemailer.config.ts
+++ app/config/nodemailer.config.ts
@@ -4,7 +4,7 @@ const { EMAIL_USER, EMAIL_PASSWORD, EMAIL_PORT, EMAIL_HOST } = process.env
 
 export const transport = createTransport({
   host: EMAIL_HOST,
-  port: parseInt(EMAIL_PORT),
+  port: Number(EMAIL_PORT),
   secure: true,
   auth: {
     user: EMAIL_USER,
--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 
-const port = parseInt(process.env.PORT ?? 5432)
+const port = Number(process.env.PORT ?? 5432)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
