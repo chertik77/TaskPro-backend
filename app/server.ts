43aad632a6ee196ac3ec3466b2ad2409b470a668
chertik77 <denysbabych.dev@gmail.com>
2026-03-24 18:46:40 +0000

    fix: minor updates & improvements

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app'
 import { env } from './config'
 
-const port = env.PORT ?? 5432
+const port = env.PORT ?? 8679
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
