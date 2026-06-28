d8afae3d993a23fa2a500e757494634e86d8713c
chertik77 <denysbabych.dev@gmail.com>
2026-06-28 16:41:44 +0100

    feat(task): make description and deadline optional and nullable

--- app/schemas/task.schema.ts
+++ app/schemas/task.schema.ts
@@ -5,24 +5,30 @@ import { objectIdSchema } from './object-id.schema'
 
 export const AddTaskSchema = z.object({
   title: z.string().min(3),
-  description: z.string().min(3),
+  description: z.optional(z.string().min(3)),
   priority: z.enum(Priority),
   labels: z.optional(z.array(objectIdSchema())),
-  deadline: z.iso.datetime().refine(value => {
-    const checkDate = new Date(value)
-    const today = new Date()
+  deadline: z
+    .optional(
+      z.iso.datetime().refine(value => {
+        const checkDate = new Date(value)
+        const today = new Date()
 
-    // Strip time down to the day level for fair comparison
-    checkDate.setHours(0, 0, 0, 0)
-    today.setHours(0, 0, 0, 0)
+        // Strip time down to the day level for fair comparison
+        checkDate.setHours(0, 0, 0, 0)
+        today.setHours(0, 0, 0, 0)
 
-    return checkDate >= today
-  }, 'Deadline must be today or in the future')
+        return checkDate >= today
+      }, 'Deadline must be today or in the future')
+    )
+    .transform(v => (v ? new Date(v) : null))
 })
 
 export const EditTaskSchema = z
   .object({
     ...AddTaskSchema.shape,
+    description: z.nullable(z.string().min(3)),
+    deadline: z.nullable(z.iso.datetime()),
     columnId: objectIdSchema()
   })
   .partial()
