c50d1f81f98ff63acfb16617d93f2a2da11ded53
chertik77 <denysbabych.dev@gmail.com>
2026-07-01 00:24:07 +0100

    feat(api): update swagger docs and switch to bearerAuth

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import type { SwaggerUiOptions } from 'swagger-ui-express'
 
 import { Router } from 'express'
 import swaggerUi from 'swagger-ui-express'
@@ -15,10 +16,26 @@ export const apiRouter = Router()
 const swaggerPath = path.join(process.cwd(), 'swagger.json')
 const swagger = JSON.parse(fs.readFileSync(swaggerPath, 'utf-8'))
 
+apiRouter.get('/swagger.json', (_, res) => {
+  res.json(swagger)
+})
+
+const swaggerOptions: SwaggerUiOptions = {
+  customSiteTitle: 'TaskPro API Docs',
+  explorer: true,
+  swaggerOptions: {
+    urls: [
+      { url: '/api/swagger.json', name: 'Main API' },
+      { url: '/api/auth/open-api/generate-schema', name: 'Auth' }
+    ],
+    'urls.primaryName': 'Main API'
+  }
+}
+
 apiRouter.use(
   '/docs',
   swaggerUi.serve,
-  swaggerUi.setup(swagger, { customSiteTitle: 'TaskPro API Docs' })
+  swaggerUi.setup(swagger, swaggerOptions)
 )
 
 apiRouter.use('/user', userRouter)
