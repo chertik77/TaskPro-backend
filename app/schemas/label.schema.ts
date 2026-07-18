09342c8c662348277a827b4c27aa40d0fa83097f
chertik77 <denysbabych.dev@gmail.com>
2026-07-18 02:28:55 +0100

    refactor(label): replace LabelColor with AccentColor enum

--- app/schemas/label.schema.ts
+++ app/schemas/label.schema.ts
@@ -1,11 +1,11 @@
-import { LabelColor } from '@prisma/client'
+import { AccentColor } from '@prisma/client'
 import * as z from 'zod'
 
 import { objectIdSchema } from './object-id.schema'
 
 export const CreateLabelSchema = z.object({
   name: z.string().min(2),
-  color: z.enum(LabelColor)
+  color: z.enum(AccentColor)
 })
 
 export const UpdateLabelSchema = CreateLabelSchema.partial()
